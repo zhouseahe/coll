6a94279c3ca7b105cdc18372e14141c47a01e7ae
zhouseahe <zhouseahe@gmail.com>
2014-05-23 14:13:08 +0800

    get cookie value by key

--- common/util.js
+++ common/util.js
@@ -9,3 +9,15 @@ exports.getId = function (arg , sperator){
         return arg.split(sperator)[1];
     }
 }
+
+exports.getCookieKey = function(cookies , key){
+    var map =  cookies.split(';');
+    var username ;
+    for(var kv  in map){
+        var p = map[kv].split('=');
+        if(key==p[0].trim()){
+           username = p[1];
+        }
+    }
+    return username;
+}
