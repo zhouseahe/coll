8777f421d0aaff07877c93b08af8dca60b7f964a
zhouseahe <zhouseahe@gmail.com>
2014-05-24 16:13:35 +0800

    connected and fetch offline msg , leave and connection removed

--- ws/manager.js
+++ ws/manager.js
@@ -2,14 +2,22 @@
  * Created by acer on 14-5-22.
  */
 // 管理 key -value :  username , connection
+
+var chatService = require('../service/chatService');
+
 var map = {};
 
 exports.put =function(username,connection){
     map[username] =connection;
+    chatService.fetchMsg(username,function(data){
+        for(var idx in  data){
+            connection.sendUTF(data[idx]);
+        }
+    })
 }
 
 exports.remove =function(username){
-    delete map.username;
+    delete map[username];
 }
 
 exports.isLive = function (username){
