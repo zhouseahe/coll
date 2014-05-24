a66762eaf22796a78ee44bb58e1ee68b0d9475e9
zhouseahe <zhouseahe@gmail.com>
2014-05-24 23:34:05 +0800

    msg parse , online create li

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -4,3 +4,30 @@
  function refresh_(){
     window.location.reload();
 }
+// check data is online users
+function isBroadUsers(msg){
+    return msg.indexOf("wsXXon")>0;
+}
+//get all users
+function parseUsers(msg){
+    var list =  msg.split("wsXXon")[1];
+    return list.split(',');
+}
+// user leave
+function isLeaveUser(msg){
+    return msg.indexOf("wsXXdn")>0;
+}
+function parseUser(msg){
+    return msg.split("wsXXdn")[1];
+}
+function generatorList(users){
+    var list = "" ;
+    for(var idx in users){
+        list += createItem(users[idx]);
+    }
+    return list;
+}
+//online user item
+function createItem(username){
+    return  "<li id='"+ username   +"' ><a href='#'" + "  id='"+ username   +"' >" + username +  "</a></li>";
+}
