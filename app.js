cbc5ef2bc6ecfd99ec8f6fb5572251195af08db3
seahezhou <zhouseahe@gmail.com>
2014-07-05 14:52:29 +0800

    cluster support mul cores

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /**
  * Module dependencies.
  */
+var cluster = require('cluster');
+var numCpus = require('os').cpus().length;
 var http = require('http');
 var app = require('./dispatcher.js').app;
 
@@ -10,14 +12,28 @@ var options = {
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./key-cert.pem')
 };*/
+
 // start httpServer
 httpServer = http.createServer(app);
-httpServer.listen(app.get('port'),function(){
-    console.log('Express server listening on port ' + app.get('port'));
-});
-httpServer.listen(app.post('port'),function(){
-    console.log('Express server listening on port ' + app.get('port'));
-});
+if(cluster.isMaster){
+    console.log(' cluster master working ....');
+    for(var i = 0 ; i < numCpus; i++){
+        cluster.fork();
+    }
+    cluster.on('death',function(worker){
+        console.log('core worker ' + worker.pid + ' died');
+    });
+}else{
+    console.log('start http servers ...');
+    httpServer.listen(app.get('port'),function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+    httpServer.listen(app.post('port'),function(){
+        console.log('Express server listening on port ' + app.get('port'));
+        console.log(process.env);
+    });
+}
+
 exports.httpServer = httpServer;
 
 //start wsServer
