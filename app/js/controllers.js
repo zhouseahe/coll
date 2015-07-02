b6fce72a735b711c8a27afc31bb71f97746f4d68
zhoushaohe <zhoushaohe@xdf.cn>
2015-07-02 21:18:10 +0800

    param

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,13 +5,15 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
         $http.get('phones/phones.json').success(function(data) {
             $scope.phones = data;
         });
-
         $scope.orderProp = 'age';
     }]);
-
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
     function($scope, $routeParams, $http) {
         $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
             $scope.phone = data;
+            $scope.mainImageUrl = data.images[0];
         });
+        $scope.setImage = function(imageUrl) {
+            $scope.mainImageUrl = imageUrl;
+        };
     }]);
