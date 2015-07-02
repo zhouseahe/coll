970e27b2d378f8bde966ff007064c01774bf2820
zhoushaohe <zhoushaohe@xdf.cn>
2015-07-02 23:28:54 +0800

    no page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,5 @@
 var phonecatControllers = angular.module('phonecatControllers', []);
+
 phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', function($scope, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
